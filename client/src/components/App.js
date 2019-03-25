7a7633ee6cefb608e1a81efc3de9fad173165773
Jahon <worldj95@gmail.com>
2019-03-24 17:11:38 -0700

    Implemented navigation bar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-const App = () => {
-  return (
-    <div>
-      Hi there! This is beginning of our React journey.
-      <div>
-        <a href="/hello">To test api routes and get response click here</a>
+import NavigationBar from './common/NavigationBar';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class App extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <NavigationBar />
+        <main className={classes.content}>
+          <a href="/hello">To test api routes and get response click here</a>
+        </main>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default App;
+export default withStyles(styles, { withTheme: true })(App);
