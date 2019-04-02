8958c3997623dcb2b6576a2b1f48feb34b6204d7
mmt09 <michaelmcdonaldtran@gmail.com>
2019-04-02 00:48:07 +0000

    unique search query working and ui fixes

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-// require('./server/listingJSON')
-
 // Connection to database
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -27,53 +25,22 @@ app.use(
     extended: true,
   })
 );
+
 app.use(bodyParser.json());
 
 //route handler
-//app.post('/api/search_apartment', (req, res) => {
-//  let result;
-//  connection.connect();
-//  connection.query(`SELECT listing_id, address, city, postal_code, amount FROM listing`,(err, rows) => {
-// WHERE postal_code=${ req.body.searchParams }`,
-//      if (err) throw err;
-//      var ObjStr = JSON.stringify(rows);
-//      var parsed = JSON.parse(ObjStr);
-//      result = parsed;
-//    }
-//  );
-//  res.send({result});
-//  connection.end();
-//});
 var globalJSON;
-connection.connect();
-connection.query(
-  'SELECT * FROM listing',
-  (err, rows) => {
-    if (err) throw err;
-    var ObjStr = JSON.stringify(rows);
-    var result = JSON.parse(ObjStr);
-    globalJSON = result;
-  }
-);
-connection.end();
-
-//route handler
 app.post('/api/search_apartment', (req, res) => {
-  res.send({ globalJSON });
+    var zip = req.body.searchParams;
+    connection.query('SELECT * FROM listing WHERE postal_code = ?', [zip], function(err, rows) {
+        if (err) throw err;
+        var ObjStr = JSON.stringify(rows);
+        var result = JSON.parse(ObjStr);
+        globalJSON = result;
+    res.send({ globalJSON });
+    });
 });
+
 //listen to this port, either server provided port or local port
 const PORT = process.env.PORT || 1337;
 app.listen(PORT);
-
-/**function displayListings(req, res, next) {
-    connection.connect();
-    connection.query('SELECT * FROM listing', (err, rows) => {
-        if(err) throw err;
-        res.send(JSON.stringify(rows));
-    });
-    connection.end();
- }
-
-app.use(express.bodyParser());
-
-app.post('/api/listings', (req, res) => {**/
