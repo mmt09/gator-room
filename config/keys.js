2cbbd82661550e8b765e02abd2341f3aa5632596
Jahon <worldj95@gmail.com>
2019-04-02 13:36:16 -0700

    reversed backif statement in keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,8 @@
 // keys.js - figure out what set of credentials to return
-if (process.env.PWD == '/home/gatorroom/csc648-sp19-team103/config') {
+if (!(module.exports = require('./dev'))) {
   // we are in production - return the prod set of keys
   module.exports = require('./prod');
 } else {
   // we are in development - return the dev keys!!!
   module.exports = require('./dev');
 }
-
