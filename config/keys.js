cae82d41f47758df2fc6afb93556b2826f9c6916
mmt09 <michaelmcdonaldtran@gmail.com>
2019-04-02 20:33:42 +0000

    keys modified

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,9 @@
 // keys.js - figure out what set of credentials to return
-if (!(module.exports = require('./dev'))) {
+if (process.env.PWD == '/home/gatorroom/csc648-sp19-team103/config') {
   // we are in production - return the prod set of keys
   module.exports = require('./prod');
 } else {
   // we are in development - return the dev keys!!!
   module.exports = require('./dev');
 }
+
