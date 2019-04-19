6c7802efe5977dc107ed524735e9c543263d568d
Jahon <worldj95@gmail.com>
2019-04-18 22:47:23 -0700

    Basic changes for Carlos page

--- client/src/components/aboutPages/CarlosVelasco/Profile.js
+++ client/src/components/aboutPages/CarlosVelasco/Profile.js
@@ -48,62 +48,61 @@ class Profile extends React.Component {
   handleDrawerToggle = () => {
     this.setState(state => ({ mobileOpen: !state.mobileOpen }));
   };
-  
+
   render() {
     const { classes } = this.props;
-    
+
     return (
       <div className={classes.root}>
         <NavigationBar />
         <main className={classes.content}>
           <div className={classes.toolbar} />
-          
+
           <Typography paragraph>
-          <div>
+            <div>
               <img
                 src="https://cdn.vox-cdn.com/thumbor/tso3z03pqxXMNvkJI__4hRk2n0Y=/0x0:1920x1080/1200x800/filters:focal(876x639:1182x945)/cdn.vox-cdn.com/uploads/chorus_image/image/60196273/NintendoSwitch_Fortnite_E3Screenshot_1.0.jpg"
-                width= "300"
-                height= "300"
-                alt="Jakhongir Khusanov"
+                width="300"
+                height="300"
+                alt="People are flying down"
                 className={classes.profileImage}
               />
-            
-            <h1>Carlos Velasco</h1>
-              <p>About Page</p>
-            
 
-            <p className="FeonaParagraph">
-              <center>
-                My name is Carlos Velasco, and I am a seinor at San Fransisco State University. This
-                is mythird year here at San Fransisco State University, and my major is Computer
-                Science. TheSome languages I have worked with are C++. C, Java, and Java Script. I
-                do not have any knowledge ofthe Python language. I have made apps in IOS and I have
-                my own website design company that Ipartner with my Father’s Graphic design company.
-                I have sold three professional websites to threeprofessional local businesses in San
-                Louis Obispo, California. When it comes to a group project Ican do either back-end
-                or front-end work, but I prefer the front-end work because I love todo designs and
-                customize the project.
-              </center>
-            </p>
-            <div>
+              <h1>Carlos Velasco</h1>
+              <p>About Page</p>
+
+              <p className="Paragraph">
+                <center>
+                  My name is Carlos Velasco, and I am a seinor at San Fransisco State University.
+                  This is mythird year here at San Fransisco State University, and my major is
+                  Computer Science. TheSome languages I have worked with are C++. C, Java, and Java
+                  Script. I do not have any knowledge ofthe Python language. I have made apps in IOS
+                  and I have my own website design company that Ipartner with my Father’s Graphic
+                  design company. I have sold three professional websites to threeprofessional local
+                  businesses in San Louis Obispo, California. When it comes to a group project Ican
+                  do either back-end or front-end work, but I prefer the front-end work because I
+                  love todo designs and customize the project.
+                </center>
+              </p>
               <div>
-                <h2>Computer Science Skills</h2>
-                <p>
-                 I have knowledge in diffrent languages of Computer Science. For instance, 
-                 I have knowledge in Notepad++, Discrete Structures, C++, Java, Machine Structures,
-                 PHP, and Bootstrap. 
-                </p>
-              </div>
+                <div>
+                  <h2>Computer Science Skills</h2>
+                  <p>
+                    I have knowledge in diffrent languages of Computer Science. For instance, I have
+                    knowledge in Notepad++, Discrete Structures, C++, Java, Machine Structures, PHP,
+                    and Bootstrap.
+                  </p>
+                </div>
               </div>
             </div>
           </Typography>
         </main>
       </div>
     );
-    }
   }
+}
 
-  Profile.propTypes = {
-    classes: PropTypes.object.isRequired,
-  };
+Profile.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 export default withStyles(styles, { withTheme: true })(Profile);
