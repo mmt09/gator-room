077d2aadb735951ab0f1564e97efdcce144399f1
Jahon <worldj95@gmail.com>
2019-05-04 21:16:20 -0700

    Fixed listing_details action to provide id parameter

--- client/src/actions/listing_details_actions.js
+++ client/src/actions/listing_details_actions.js
@@ -2,9 +2,11 @@
 import axios from 'axios';
 import { FETCH_LISTING_DETAILS } from './types';
 
-export const fetchListingDetails = () => async dispatch => {
+export const fetchListingDetails = listingID => async dispatch => {
   try {
-    const { data } = await axios.post('/api/listing_details');
+    const { data } = await axios.post('/api/listing_details', {
+      listingID,
+    });
     dispatch({ type: FETCH_LISTING_DETAILS, payload: data });
   } catch (error) {
     // eslint-disable-next-line no-console
