8fec1224f9a65d69b543b3ae2cced29a05688645
Jahon <worldj95@gmail.com>
2019-05-04 22:30:58 -0700

    Rendered basic listing info and fixed default state for its reducer

--- client/src/reducers/listing_details_reducer.js
+++ client/src/reducers/listing_details_reducer.js
@@ -1,6 +1,6 @@
 import { FETCH_LISTING_DETAILS } from '../actions/types';
 
-export default function(state = null, action) {
+export default function(state = [], action) {
   switch (action.type) {
     case FETCH_LISTING_DETAILS:
       return action.payload;
