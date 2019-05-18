58849ec8e4714d2357713e6e8abc9bde307eaaa5
Jahon <worldj95@gmail.com>
2019-05-18 16:40:40 -0700

    Added eddit and save button logic

--- client/src/views/portals/UserProfileCard.js
+++ client/src/views/portals/UserProfileCard.js
@@ -9,6 +9,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import Paper from '@material-ui/core/Paper';
+import CustomInput from 'components/CustomInput/CustomInput.jsx';
 
 const styles = theme => ({
   butt: {
@@ -41,43 +42,97 @@ const styles = theme => ({
     height: 100,
     display: 'flex',
   },
+  textArea: {},
 });
 
 // This an entry point to our components
-const UserProfileCard = props => {
-  // console.log(this.props.search);
-  const { classes, firstName, lastName, phone, picture, email, landlordID } = props;
+class UserProfileCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputDisabled: true,
+    };
+    this.editButtonPress = this.editButtonPress.bind(this);
+  }
 
-  return (
-    <div className={classes.root}>
-      <Card className={classes.card}>
-        <CardContent>
-          <Avatar alt="Nico Graves" src={picture} className={classes.avatar} />
-          <Typography variant="h6" component="h2">
-            {firstName} {lastName}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {landlordID ? 'Landlord' : 'Student'}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {email}
-          </Typography>
-          <Typography className={classes.pos} color="textSecondary">
-            {phone}
-          </Typography>
-          <Paper elevation={0.5}>
-            <Typography component="p">{`Hello! My name is ${firstName} and I am a senior student at San Francisco State. We have a lovely spot open in our house that is just waiting to be filled by you. The house is very relaxed and we're looking for a chill roomate to come live with us. Feel free to email me, thanks!`}</Typography>
-          </Paper>
-        </CardContent>
-        <CardActions>
-          <Button variant="contained" className={classes.butt}>
-            Edit
-          </Button>
-        </CardActions>
-      </Card>
-    </div>
-  );
-};
+  editButtonPress() {
+    const { inputDisabled } = this.state;
+    this.setState({ inputDisabled: !inputDisabled });
+  }
+
+  renderButton() {
+    const { classes } = this.props;
+    const { inputDisabled } = this.state;
+    if (inputDisabled) {
+      return (
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.butt}
+          onClick={this.editButtonPress}
+        >
+          Edit
+        </Button>
+      );
+    }
+
+    return (
+      <Button
+        variant="contained"
+        color="secondary"
+        className={classes.butt}
+        onClick={this.editButtonPress}
+      >
+        Save
+      </Button>
+    );
+  }
+
+  render() {
+    // console.log(this.props.search);
+    const { classes, firstName, lastName, phone, picture, email, landlordID } = this.props;
+    const { inputDisabled } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Avatar alt="Nico Graves" src={picture} className={classes.avatar} />
+            <Typography variant="h6" component="h2">
+              {firstName} {lastName}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {landlordID ? 'Landlord' : 'Student'}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {email}
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+              {phone}
+            </Typography>
+            <Paper elevation={0.5}>
+              <CustomInput
+                labelText="About you"
+                id="message"
+                formControlProps={{
+                  fullWidth: true,
+                  className: classes.textArea,
+                }}
+                inputProps={{
+                  multiline: true,
+                  rows: 5,
+                  placeholder: 'Enter a brief information about you',
+                  disabled: inputDisabled,
+                }}
+              />
+            </Paper>
+          </CardContent>
+          <CardActions>{this.renderButton()}</CardActions>
+        </Card>
+      </div>
+    );
+  }
+}
 
 UserProfileCard.propTypes = {
   classes: PropTypes.object.isRequired,
