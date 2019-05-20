3f5e55ea544b7534e7592e171e10aab8558f67c7
mmt09 <michaelmcdonaldtran@gmail.com>
2019-05-19 18:34:19 -0700

    added geocoder to listing upload query

--- routes/listingUploadRoutes.js
+++ routes/listingUploadRoutes.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql');
-const geocoder = require('node-geocoder');
+const NodeGeocoder = require('node-geocoder');
 const keys = require('../config/keys');
+
+const options = {
+  provider: 'google',
+  apiKey: 'AIzaSyCKSxjOsN5bYFi9QtYqbrLoPtknm8yr5E0',
+  formatter: null,
+};
+const geocoder = NodeGeocoder(options);
 
 // Connection to database
 // Production keys are stored in config directory, local dev keys are not pushed to server
@@ -12,8 +19,16 @@ const connection = mysql.createConnection({
 });
 
 module.exports = app => {
-  app.post('/api/listingUpload', (req, res) => {
+  app.post('/api/listingUpload', async (req, res) => {
     const { streetAddress, city, zip, bedroom, bathroom, kitchen, price, description } = req.body;
+    // let lat;
+    // let long;
+
+    await geocoder.geocode(streetAddress, (err, res) => {
+      lat = res[0].latitude;
+      long = res[0].longitude;
+    });
+
     connection.query(
       'INSERT INTO listing (address, city, postal_code, num_bedroom, num_bathroom, num_kitchen, amount, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
       [streetAddress, city, zip, bedroom, bathroom, kitchen, price, description],
