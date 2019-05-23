e3d413f62e915ba6759b8b3396d11aafab952c20
Jahon <worldj95@gmail.com>
2019-05-23 10:32:18 -0700

    Redirect user

--- services/passport.js
+++ services/passport.js
@@ -32,7 +32,7 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: 'http://gatorroom.xyz/auth/google/callback',
+      callbackURL: '/auth/google/callback',
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
