1f2fe861a42a97fde448a74ed4dccdcad98ca442
Jahon <worldj95@gmail.com>
2019-05-23 00:20:22 -0700

    Render listings for the landlord

--- client/src/views/portals/ListingList.js
+++ client/src/views/portals/ListingList.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
@@ -8,6 +9,11 @@ import Button from '@material-ui/core/Button';
 import { Link as RouterLink } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
+import GridContainer from 'components/Grid/GridContainer.jsx';
+import GridItem from 'components/Grid/GridItem.jsx';
+import ListingInfoCard from 'views/common/ListingInfoCard.js';
+import { Redirect, withRouter } from 'react-router';
+
 import * as actions from '../../actions';
 
 const styles = theme => ({
@@ -27,18 +33,69 @@ const styles = theme => ({
 });
 
 class ListingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toListing: false,
+      listingID: null,
+    };
+  }
+
   componentDidMount() {
     // Below two lines Can be deleted once development is done
     const { fetchLandlordListings, landlordID } = this.props;
     fetchLandlordListings(landlordID);
   }
 
+  setListing = listingID => {
+    this.setState({ toListing: true, listingID });
+  };
+
+  renderListing = () => {
+    const { landlordListings, classes } = this.props;
+    if (landlordListings) {
+      return landlordListings.map(listing => (
+        <GridItem xs={12} sm={12} md={7} key={listing.listing_id} height="">
+          <div className={classes.adminPaper}>
+            <ListingInfoCard
+              key={listing.listing_id}
+              picture={listing.picture}
+              city={listing.city}
+              address={listing.address}
+              price={listing.amount}
+              numberOfBedroom={listing.num_bedroom}
+              numberOfBathroom={listing.num_bathroom}
+              imageOne={listing.image_1}
+              approved={listing.approved}
+              onClick={() => this.setListing(listing.listing_id)}
+            />
+          </div>
+        </GridItem>
+      ));
+    }
+    return null;
+  };
+
   render() {
-    const { classes, landlordID } = this.props;
+    const { classes, landlordListings } = this.props;
+    console.log(landlordListings);
+
+    const { toListing, listingID } = this.state;
+
+    if (toListing === true) {
+      return <Redirect push to={`/listings/${listingID}`} />;
+    }
     return (
       <Card className={classes.root}>
         <List>
-          <ListItem>{landlordID}</ListItem>
+          <div className={classes.adminSection}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={8}>
+                <h2 className={classes.title}>Your Listings</h2>
+              </GridItem>
+            </GridContainer>
+            <GridContainer justify="center">{this.renderListing()}</GridContainer>
+          </div>
 
           <ListItem className={classes.booty}>
             <Button
@@ -69,4 +126,4 @@ function mapStateToProps({ landlordListings }) {
 export default connect(
   mapStateToProps,
   actions
-)(withStyles(styles)(ListingList));
+)(withRouter(withStyles(styles)(ListingList)));
